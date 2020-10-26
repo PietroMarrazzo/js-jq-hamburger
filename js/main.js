40cfa5af8b6512bd61392e55eca7044ac6bd4057
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-26 15:05:54 +0100

    esercizi vari
    provato l'esercizio aggiungendo toggle, slideDown, fadeIn

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
 // acquisisco le variabili che interagiranno tra loro
 // hamburger da clickare
 // hamburgerMenu da far comparire
+// hamburgerClose per nascondere
 var hamburger = $('.header-right a i');
 var hamburgerMenu = $('.hamburger-menu');
 var hamburgerClose = $('.hamburger-menu .close');
 
+// esercizio con aggiunta classi
+// hamburger.click(function() {
+//     hamburgerMenu.addClass('active');
+// })
+
+// hamburgerClose.click(function() {
+//     hamburgerMenu.removeClass('active');
+// })
+
+
+// esercizio con show/hide
+// hamburger.click(function() {
+//     hamburgerMenu.show(1500);
+// })
+
+// hamburgerClose.click(function() {
+//     hamburgerMenu.hide(1500);
+// })
+
+
+// esercizio con fadeIn/fadeOut
+// hamburger.click(function() {
+//     hamburgerMenu.fadeIn(1500);
+// })
+
+// hamburgerClose.click(function() {
+//     hamburgerMenu.fadeOut(1500);
+// })
+
+
+// esercizio con toogle
+// hamburger.click(function() {
+//     hamburgerMenu.Toggle(1500);
+// })
+
+// hamburgerClose.click(function() {
+//     hamburgerMenu.Toggle(1500);
+// })
+
+
+// esercizio con slideUp/Down
 hamburger.click(function() {
-    hamburgerMenu.addClass('active');
+    hamburgerMenu.slideDown(1500);
 })
 
 hamburgerClose.click(function() {
-    hamburgerMenu.removeClass('active');
+    hamburgerMenu.slideUp(1500);
 })
